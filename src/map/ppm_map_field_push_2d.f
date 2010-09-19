93d675be44f239594b5f1b21edac7ce52eb01951
oawile <oawile@7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9>
2010-09-19 09:31:53 +0000

    - added copyright notice and legal stuff
    git-svn-id: https://ppm.inf.ethz.ch/svn/ppmcore/trunk/libppm@720 7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9

--- src/map/ppm_map_field_push_2d.f
+++ src/map/ppm_map_field_push_2d.f
@@ -1,9 +1,30 @@
       !-------------------------------------------------------------------------
       !  Subroutine   :               ppm_map_field_push_2d
       !-------------------------------------------------------------------------
-      !  Parallel Particle Mesh Library (PPM)
-      !  ETH Zurich
-      !  CH-8092 Zurich, Switzerland
+      ! Copyright (c) 2010 CSE Lab (ETH Zurich), MOSAIC Lab (ETH Zurich), 
+      !                    Center for Fluid Dynamics (DTU)
+      !
+      !
+      ! This file is part of the Parallel Particle Mesh Library (PPM).
+      !
+      ! PPM is free software: you can redistribute it and/or modify
+      ! it under the terms of the GNU Lesser General Public License 
+      ! as published by the Free Software Foundation, either 
+      ! version 3 of the License, or (at your option) any later 
+      ! version.
+      !
+      ! PPM is distributed in the hope that it will be useful,
+      ! but WITHOUT ANY WARRANTY; without even the implied warranty of
+      ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+      ! GNU General Public License for more details.
+      !
+      ! You should have received a copy of the GNU General Public License
+      ! and the GNU Lesser General Public License along with PPM. If not,
+      ! see <http://www.gnu.org/licenses/>.
+      !
+      ! Parallel Particle Mesh Library (PPM)
+      ! ETH Zurich
+      ! CH-8092 Zurich, Switzerland
       !-------------------------------------------------------------------------
 
 
