9f8926acfa127d2bad149105d9f278edba1150b5
oawile <oawile@7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9>
2011-03-29 17:32:13 +0000

    updated python visualization script
    The new ppmdbg.py supports the updated output format produced
    by ppm_dbg_print.
    
    git-svn-id: https://ppm.inf.ethz.ch/svn/ppmcore/trunk/libppm@805 7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9

--- utils/ppmdbg.py
+++ utils/ppmdbg.py
@@ -23,8 +23,16 @@ def sub2rect(minc,maxc):
     y1 = minc[1]
     x2 = maxc[0]
     y2 = maxc[1]
-    f = np.array([(x1,y1),(x2,y1),(x2,y2),(x1,y2)])
-    return f
+    return np.array([(x1,y1),(x2,y1),(x2,y2),(x1,y2)])
+
+def gl2rect(minc,maxc,bc,halo):
+    gminc = [0.0]*2
+    gmaxc = [0.0]*2
+    gminc[0] = minc[0] - halo if bc[0] == 1 else minc[0]
+    gmaxc[0] = maxc[0] + halo if bc[1] == 1 else maxc[0]
+    gminc[1] = minc[1] - halo if bc[2] == 1 else minc[1]
+    gmaxc[1] = maxc[1] + halo if bc[3] == 1 else maxc[1]
+    return sub2rect(gminc,gmaxc)
 
 
 def sub2cube(minc,maxc):
@@ -35,10 +43,6 @@ def sub2cube(minc,maxc):
     y2 = maxc[1]
     z2 = maxc[2]
 
-    sx = x2 - x1
-    sy = y2 - y1
-    sz = z2 - z1
-   
     # front face
     f1x = np.array([x1,x2])
     f1y = np.array([[y1,y1],[y1,y1]])
@@ -79,6 +83,17 @@ def sub2cube(minc,maxc):
     return [(f1x,f1y,f1z),(f2x,f2y,f2z),(f3x,f3y,f3z),(f4x,f4y,f4z),\
             (f5x,f5y,f5z),(f6x,f6y,f6z)]
 
+def gl2cube(minc,maxc,bc,halo):
+    gminc = [0.0]*3
+    gmaxc = [0.0]*3
+    gminc[0] = minc[0] - halo if bc[0] == 1 else minc[0]
+    gmaxc[0] = maxc[0] + halo if bc[1] == 1 else maxc[0]
+    gminc[1] = minc[1] - halo if bc[2] == 1 else minc[1]
+    gmaxc[1] = maxc[1] + halo if bc[3] == 1 else maxc[1]
+    gminc[2] = minc[2] - halo if bc[4] == 1 else minc[2]
+    gmaxc[2] = maxc[2] + halo if bc[5] == 1 else maxc[2]
+    return sub2cube(gminc,gmaxc)
+
 def plotsub3(ax,f,cpu):
     nc = len(cmap.keys())
     for i in range(6):
@@ -86,7 +101,7 @@ def plotsub3(ax,f,cpu):
 
 def plotgl3(ax,gl):
     for i in range(6):
-        ax.plot_surface(gl[i][0],gl[i][1],gl[i][2],alpha=0.01,\
+        ax.plot_surface(gl[i][0],gl[i][1],gl[i][2],alpha=0.02,\
                 color='k',linewidth=0) 
 
 def plotsub2(ax,f,cpu):
@@ -120,7 +135,6 @@ def main():
     print dim
     l2 = subfile.readline()
     halo = float(l2.strip())
-    
     if dim == 2:
         ax = fig.add_subplot(111)
         for l in subfile:
@@ -128,9 +142,9 @@ def main():
             min_sub = [float(r[0]),float(r[1])]
             max_sub = [float(r[2]),float(r[3])]
             proc = int(r[4])
+            bc = [int(r[5]),int(r[6]),int(r[7]),int(r[8])]
             if (halo > 0.0):
-                glfaces = sub2rect([min_sub[0]-halo,min_sub[1]-halo], \
-                        [max_sub[0]+halo,max_sub[1]+halo])
+                glfaces = gl2rect(min_sub,max_sub,bc,halo)
                 plotgl2(ax,glfaces)
             faces = sub2rect(min_sub,max_sub)
             plotsub2(ax,faces,proc)
@@ -141,14 +155,14 @@ def main():
             min_sub = [float(r[0]),float(r[1]),float(r[2])]
             max_sub = [float(r[3]),float(r[4]),float(r[5])]
             proc = int(r[6])
+            bc = [int(r[7]),int(r[8]),int(r[9]),\
+                    int(r[10]),int(r[11]),int(r[12])]
             if (halo > 0.0):
-                glfaces = sub2cube([min_sub[i]-halo for i in range(len(min_sub))], \
-                        [max_sub[i]+halo for i in range(len(max_sub))])
+                glfaces = gl2cube(min_sub,max_sub,bc,halo)
                 plotgl3(ax,glfaces)
             faces = sub2cube(min_sub,max_sub)
             plotsub3(ax,faces,proc)
     subfile.close()
-
     datfile = open(datfilen)
     x = []
     y = []
