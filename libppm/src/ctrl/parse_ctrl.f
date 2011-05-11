2d1d6e001b74de41e4e30a4d697e0d19734665de
milanm <milanm@7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9>
2011-05-11 15:03:42 +0000

    Refactored ppm_module_ctrl and added more data types.
    git-svn-id: https://ppm.inf.ethz.ch/svn/ppmcore/trunk@867 7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9

--- libppm/src/ctrl/parse_ctrl.f
+++ libppm/src/ctrl/parse_ctrl.f
@@ -3,16 +3,19 @@
              IF (WRAP(DTYPE)_args(i)%ctrl_name_set) THEN
                 cvar = WRAP(DTYPE)_args(i)%ctrl_name &
                      (1:LEN_TRIM(WRAP(DTYPE)_args(i)%ctrl_name))
+                current_var = cvar
                 CALL UpperCase(cvar, LEN_TRIM(cvar), info)
                 IF (carg .EQ. cvar) THEN
                    IF (.NOT. WRAP(DTYPE)_args(i)%clf_supplied) THEN
-#if defined(STRING) && !defined(ARRAY)
+#ifdef __STRING
                       WRAP(DTYPE)_args(i)%variable = ADJUSTL(cvalue)
 #else
-                      READ (cvalue, *, IOSTAT=ios, ERR=200) WRAP(DTYPE)_args(i)%variable
+                      READ (cvalue, *, IOSTAT=ios, ERR=300) WRAP(DTYPE)_args(i)%variable
 #endif
                    END IF
                    CYCLE var_loop
                 END IF
              END IF
           END DO
+#undef DTYPE
+#undef __STRING
--- libppm/src/ctrl/validate.f
+++ libppm/src/ctrl/validate.f
@@ -2,10 +2,12 @@
        DO i=1,WRAP(DTYPE)_args_i
           IF (ASSOCIATED(WRAP(DTYPE)_args(i)%validator)) THEN
              IF (.NOT. WRAP(DTYPE)_args(i)%validator(WRAP(DTYPE)_args(i)%variable)) THEN
-                WRITE (*,*) 'Argument ', WRAP(DTYPE)_args(i)%name(1:LEN_TRIM(WRAP(DTYPE)_args(i)%name)), &
+                WRITE (cvar,*) 'Argument ', WRAP(DTYPE)_args(i)%name(1:LEN_TRIM(WRAP(DTYPE)_args(i)%name)), &
                      ' fails validator check!'
-                info = 1
+                info = ppm_error_fatal
+                CALL ppm_error(ppm_err_argument, caller, cvar, __LINE__, info)
                 GOTO 9999
              END IF
           END IF
        END DO
+#undef DTYPE
