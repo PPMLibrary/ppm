26ec2d9f8ef9fa9fe77912c6280491c767d43a02
Sylvain Reboux <sylvain.reboux@inf.ethz.ch>
2011-05-31 08:23:45 +0200

    updated default parameters for sop

--- src/sop/ppm_sop_helpers.f
+++ src/sop/ppm_sop_helpers.f
@@ -25,16 +25,21 @@ SUBROUTINE sop_init_opts(opts,info)
 
     opts%level_set = .FALSE.
     opts%scale_D = 1._MK
+    opts%rcp_over_D = 2._MK
     opts%nb_width = 1._MK
     opts%nb_width2 = 1._MK
-    opts%adaptivity_criterion = 1._MK
-    opts%fuse_radius = 0._MK
+    opts%adaptivity_criterion = 8._MK
+    opts%attractive_radius0 = 0.4_MK
+    opts%fuse_radius = 0.2_MK
     opts%param_nb = 1._MK
     opts%diff_eq => NULL()
     opts%order_approx = 4
     opts%maximum_D = 1._MK
     opts%minimum_D = 0.01_MK     
     opts%nb_grad_desc_steps = 0
+    opts%nneigh_theo = 24
+    opts%nneigh_critical = 20
+    opts%nneigh_toobig = 2000
 
     9999 CONTINUE
 
