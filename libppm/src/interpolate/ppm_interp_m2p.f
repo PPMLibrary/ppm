5660346678f1824661b492675e0968d68cb9ee28
oawile <oawile@7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9>
2011-06-16 13:24:02 +0000

    - the fields passed to these routines must be POINTERs because   else the lower/upper bounds are wrong. The bounds for fields are   always 1-ghostsize:Nm+ghostsize, but this is not preserved for   INTENT(IN) attributed array arguments. Thanks Sylvain!
    git-svn-id: https://ppm.inf.ethz.ch/svn/ppmcore/trunk/libppm@914 7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9

--- libppm/src/interpolate/ppm_interp_m2p.f
+++ libppm/src/interpolate/ppm_interp_m2p.f
@@ -105,9 +105,9 @@
       REAL(MK) , DIMENSION(:)         , POINTER        :: up
       !!! particle weights onto which to interpolate
 #if   __DIME == __2D
-      REAL(MK) , DIMENSION(:,:,:    ) , INTENT(IN   )  :: field_up
+      REAL(MK) , DIMENSION(:,:,:    ) , POINTER        :: field_up
 #elif __DIME == __3D
-      REAL(MK) , DIMENSION(:,:,:,:  ) , INTENT(IN   )  :: field_up
+      REAL(MK) , DIMENSION(:,:,:,:  ) , POINTER        :: field_up
 #endif
       !!! field from which to interpolate
 #elif __MODE == __VEC
@@ -116,9 +116,9 @@
       REAL(MK) , DIMENSION(:,:)       , POINTER        :: up
       !!! particle weights onto which to interpolate
 #if   __DIME == __2D
-      REAL(MK) , DIMENSION(:,:,:,:  ) , INTENT(IN   )  :: field_up
+      REAL(MK) , DIMENSION(:,:,:,:  ) , POINTER        :: field_up
 #elif __DIME == __3D
-      REAL(MK) , DIMENSION(:,:,:,:,:) , INTENT(IN   )  :: field_up
+      REAL(MK) , DIMENSION(:,:,:,:,:) , POINTER        :: field_up
 #endif
       !!! field from which to interpolate
 #endif
