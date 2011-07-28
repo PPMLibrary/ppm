7db832d534822ba62dbc1d17689cbe92a9e3c100
oawile <oawile@7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9>
2011-07-28 14:04:36 +0000

    Fix bug, missing USE mmodule ... related to ppm_topo_alloc and ppm_topo_check_id
    git-svn-id: https://ppm.inf.ethz.ch/svn/ppmcore/trunk/libppm@1104 7c7fe9aa-52eb-4d9e-b0a8-ba7d787348e9

--- libppm/src/map/ppm_map_connect_prune.f
+++ libppm/src/map/ppm_map_connect_prune.f
@@ -41,6 +41,7 @@
       USE ppm_module_error
       USE ppm_module_alloc
       USE ppm_module_util_invert_list
+      USE ppm_module_check_id
       IMPLICIT NONE
 
       !-------------------------------------------------------------------------
