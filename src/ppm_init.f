9d912c1bbf4e68fa8748915ca7b4ff9eabf91c74
Omar Awile <oawile@cbl1.inf.ethz.ch>
2011-10-17 21:17:56 +0200

    bugfix: initialize I/O only after MPI is initialized.

--- src/ppm_init.f
+++ src/ppm_init.f
@@ -110,19 +110,6 @@
           ilog = -1
       ENDIF
  
-      !-------------------------------------------------------------------------
-      !  Set unit numbers of stdout and stderr and log file
-      !-------------------------------------------------------------------------
-      CALL ppm_io_set_unit(istdout,istderr,ilog,info)
-      
-      !-------------------------------------------------------------------------
-      !  Now the library can talk...
-      !-------------------------------------------------------------------------
-      
-      !-------------------------------------------------------------------------
-      !  Initialise 
-      !-------------------------------------------------------------------------
-      CALL substart('ppm_init',t0,info)
 
       !-------------------------------------------------------------------------
       !  Check arguments
@@ -139,7 +126,31 @@
       CALL MPI_Initialized(ppm_mpi_init,info)
 
       !-------------------------------------------------------------------------
-      !  If not exit with error status ppm_error_fatal
+      !  Get the MPI communicator
+      !-------------------------------------------------------------------------
+      ppm_comm = comm
+      CALL MPI_Comm_Size(ppm_comm,ppm_nproc,info)
+      CALL MPI_Comm_Rank(ppm_comm,ppm_rank,info)
+      CALL MPI_Get_Processor_Name(cbuf,ilen,info)
+#else
+      ppm_nproc = 1
+      ppm_rank  = 0
+#endif
+      
+      !-------------------------------------------------------------------------
+      !  Set unit numbers of stdout and stderr and log file
+      !-------------------------------------------------------------------------
+      CALL ppm_io_set_unit(istdout,istderr,ilog,info)
+      
+      !-------------------------------------------------------------------------
+      !  Now the library can talk...
+      !-------------------------------------------------------------------------
+      
+      CALL substart('ppm_init',t0,info)
+
+#if defined __MPI
+      !-------------------------------------------------------------------------
+      !  If MPI not initialized exit with error status ppm_error_fatal
       !-------------------------------------------------------------------------
       IF (.NOT.ppm_mpi_init) THEN
          info = ppm_error_fatal
@@ -147,14 +158,6 @@
      &         'Call MPI_Init before calling ppm_init !',__LINE__,info)
          GOTO 9999
       ENDIF
-
-      !-------------------------------------------------------------------------
-      !  Get the MPI communicator
-      !-------------------------------------------------------------------------
-      ppm_comm = comm
-      CALL MPI_Comm_Size(ppm_comm,ppm_nproc,info)
-      CALL MPI_Comm_Rank(ppm_comm,ppm_rank,info)
-      CALL MPI_Get_Processor_Name(cbuf,ilen,info)
       IF (ppm_debug .GT. 0) THEN
         WRITE(mesg,'(2A)') '*** This is the PPM library starting on ',&
      &                    cbuf(1:ilen)
@@ -169,8 +172,6 @@
         ENDIF
       ENDIF
 #else
-      ppm_nproc = 1
-      ppm_rank  = 0
       WRITE(mesg,'(A)') '*** This is the PPM library in single-processor mode'
       CALL ppm_log('ppm_init',mesg,info)
       CALL ppm_write(ppm_rank,'ppm_init',mesg,info)
