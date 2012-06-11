a444e0bcccb9a88e8e27851e517d4f0d104a172f
Sylvain Reboux <sylvain.reboux@inf.ethz.ch>
2012-06-11 16:09:38 +0200

    list of fields for vtk output

--- src/vtk/ppm_vtk_particles.f
+++ src/vtk/ppm_vtk_particles.f
@@ -1,5 +1,5 @@
          SUBROUTINE DTYPE(ppm_vtk_particles)(filename, Pc, info, &
-              step, with_ghosts, with_nvlist, wp_list,wp_field_list)
+              step, with_ghosts, with_nvlist, Field_list)
            !--------------------------------------------------------------------
            !  Arguments
            !--------------------------------------------------------------------
@@ -10,9 +10,12 @@
            INTEGER,               OPTIONAL, INTENT(IN   ) :: step
            LOGICAL,               OPTIONAL, INTENT(IN   ) :: with_ghosts
            LOGICAL,               OPTIONAL, INTENT(IN   ) :: with_nvlist
-           INTEGER, DIMENSION(:), OPTIONAL, INTENT(IN   ) :: wp_list
+           CLASS(ppm_v_main_abstr),OPTIONAL               :: Field_list
+           !!!list of fields to print out (default = ALL)
+
+           !INTEGER, DIMENSION(:), OPTIONAL, INTENT(IN   ) :: wp_list
            !list of properties to print out (default = ALL)
-           INTEGER, DIMENSION(:), OPTIONAL, INTENT(IN   ) :: wp_field_list
+           !INTEGER, DIMENSION(:), OPTIONAL, INTENT(IN   ) :: wp_field_list
            !list of properties to print out as vector field (default = NONE)
            !--------------------------------------------------------------------
            !  Variables
@@ -31,6 +34,9 @@
            INTEGER, DIMENSION(:),   POINTER     :: wpi  => NULL()
 
            CLASS(DTYPE(ppm_t_part_prop)_),POINTER :: prop => NULL()
+           CLASS(ppm_t_discr_data),       POINTER :: discr_data => NULL()
+           CLASS(ppm_t_field_),           POINTER :: field => NULL()
+           CLASS(ppm_t_main_abstr),       POINTER :: el => NULL()
 
            start_subroutine("ppm_vtk_particles")
            
@@ -70,122 +76,151 @@
 
            ! create the list of properties to print
            ! integer property
-           IF(PRESENT(wp_list)) THEN
-              nb_wp=SIZE(wp_list)
+           !IF(PRESENT(wp_list)) THEN
+              !nb_wp=SIZE(wp_list)
 
-              ALLOCATE(wpi_l(nb_wp),wps_l(nb_wp),wpv_l(nb_wp),STAT=info)
-              !find how many integer/scalar/vector properties are to be
-              !printed out
-              DO i=1,nb_wp
-                 ii = wp_list(i)
-                 IF (.NOT.Pc%props%exists(ii)) THEN
-                    info = ppm_error_error
-                    CALL ppm_error(ppm_err_argument,caller,   &
-                 &  'invalid property for printout. Data may not be allocated',&
-                         &  __LINE__,info)
-                    GOTO 9999
-                 ENDIF
-                 prop => Pc%props%vec(ii)%t
-                 IF (.NOT.prop%flags(ppm_ppt_partial)) THEN
-                    info = ppm_error_error
-                    CALL ppm_error(ppm_err_argument,caller,   &
-                         &  'trying to printout a property that is not mapped &
-                         & to the particles',&
-                         &  __LINE__,info)
-                    GOTO 9999
-                 ENDIF
-                 SELECT CASE (prop%data_type)
-                 CASE (ppm_type_int)
-                     IF (prop%lda.NE.1) THEN
-                         info = ppm_error_error
-                         CALL ppm_error(ppm_err_argument,caller,   &
-                     &  'no support for printout of vector integer properties',&
-                             &  __LINE__,info)
-                         GOTO 9999
-                     ENDIF
-                     nb_wpi = nb_wpi + 1
-                     wpi_l(nb_wpi) = ii
+              !ALLOCATE(wpi_l(nb_wp),wps_l(nb_wp),wpv_l(nb_wp),STAT=info)
+              !!find how many integer/scalar/vector properties are to be
+              !!printed out
+              !DO i=1,nb_wp
+                 !ii = wp_list(i)
+                 !IF (.NOT.Pc%props%exists(ii)) THEN
+                    !info = ppm_error_error
+                    !CALL ppm_error(ppm_err_argument,caller,   &
+                 !&  'invalid property for printout. Data may not be allocated',&
+                         !&  __LINE__,info)
+                    !GOTO 9999
+                 !ENDIF
+                 !prop => Pc%props%vec(ii)%t
+                 !IF (.NOT.prop%flags(ppm_ppt_partial)) THEN
+                    !info = ppm_error_error
+                    !CALL ppm_error(ppm_err_argument,caller,   &
+                         !&  'trying to printout a property that is not mapped &
+                         !& to the particles',&
+                         !&  __LINE__,info)
+                    !GOTO 9999
+                 !ENDIF
+                 !SELECT CASE (prop%data_type)
+                 !CASE (ppm_type_int)
+                     !IF (prop%lda.NE.1) THEN
+                         !info = ppm_error_error
+                         !CALL ppm_error(ppm_err_argument,caller,   &
+                     !&  'no support for printout of vector integer properties',&
+                             !&  __LINE__,info)
+                         !GOTO 9999
+                     !ENDIF
+                     !nb_wpi = nb_wpi + 1
+                     !wpi_l(nb_wpi) = ii
 
-                 CASE (ppm_type_real)
-                     IF (prop%lda.EQ.1) THEN
-                         nb_wps = nb_wps + 1
-                         wps_l(nb_wps) = ii
-                     ELSE
-                         nb_wpv = nb_wpv + 1
-                         wpv_l(nb_wpv) = ii
-                     ENDIF
-                 CASE DEFAULT
-                     info = ppm_error_error
-                     CALL ppm_error(ppm_err_argument,caller,   &
-                 &  'no support for vtk printout of data other than real or int',&
-                         &  __LINE__,info)
-                     GOTO 9999
-                 END SELECT
-                 prop => NULL()
-              ENDDO
+                 !CASE (ppm_type_real)
+                     !IF (prop%lda.EQ.1) THEN
+                         !nb_wps = nb_wps + 1
+                         !wps_l(nb_wps) = ii
+                     !ELSE
+                         !nb_wpv = nb_wpv + 1
+                         !wpv_l(nb_wpv) = ii
+                     !ENDIF
+                 !CASE DEFAULT
+                     !info = ppm_error_error
+                     !CALL ppm_error(ppm_err_argument,caller,   &
+                 !&  'no support for vtk printout of data other than real or int',&
+                         !&  __LINE__,info)
+                     !GOTO 9999
+                 !END SELECT
+                 !prop => NULL()
+              !ENDDO
 
-           ELSE
-              nb_wp = 0
-           ENDIF
+           !ELSE
+              !nb_wp = 0
+           !ENDIF
 
 
-           ! vector field property
-           IF(PRESENT(wp_field_list)) THEN
-              nb_wp_field=SIZE(wp_field_list)
-              ALLOCATE(wp_field_l(nb_wp_field),STAT=info)
-              wp_field_l=wp_field_list
+           !! vector field property
+           !IF(PRESENT(wp_field_list)) THEN
+              !nb_wp_field=SIZE(wp_field_list)
+              !ALLOCATE(wp_field_l(nb_wp_field),STAT=info)
+              !wp_field_l=wp_field_list
 
-              !Check that the listed properties are indeed vector fields,
-              ! are allocated and mapped to particles
-              DO i=1,nb_wp_field
-                 ii = wp_field_list(i)
-                 IF (.NOT.Pc%props%exists(ii)) THEN
-                    info = ppm_error_error
-                    CALL ppm_error(ppm_err_argument,caller,   &
-                 &  'invalid property for printout. Data may not be allocated',&
-                         &  __LINE__,info)
-                    GOTO 9999
-                 ENDIF
-                 prop => Pc%props%vec(ii)%t
-                 IF (.NOT.prop%flags(ppm_ppt_partial)) THEN
-                    info = ppm_error_error
-                    CALL ppm_error(ppm_err_argument,caller,   &
-                         &  'trying to printout a property that is not mapped &
-                         & to the particles',&
-                         &  __LINE__,info)
-                    GOTO 9999
-                 ENDIF
-                 IF (.NOT. prop%data_type.EQ.ppm_type_real .OR. &
-                        prop%lda.LE.1) THEN 
-                         info = ppm_error_error
-                         CALL ppm_error(ppm_err_argument,caller,   &
-                     &  'property should be a real vector field',&
-                             &  __LINE__,info)
-                         GOTO 9999
-                 ENDIF
-                 prop => NULL()
-              ENDDO
-           ELSE
-              ! dont print anything
-              nb_wp_field = 0
-           ENDIF
+              !!Check that the listed properties are indeed vector fields,
+              !! are allocated and mapped to particles
+              !DO i=1,nb_wp_field
+                 !ii = wp_field_list(i)
+                 !IF (.NOT.Pc%props%exists(ii)) THEN
+                    !info = ppm_error_error
+                    !CALL ppm_error(ppm_err_argument,caller,   &
+                 !&  'invalid property for printout. Data may not be allocated',&
+                         !&  __LINE__,info)
+                    !GOTO 9999
+                 !ENDIF
+                 !prop => Pc%props%vec(ii)%t
+                 !IF (.NOT.prop%flags(ppm_ppt_partial)) THEN
+                    !info = ppm_error_error
+                    !CALL ppm_error(ppm_err_argument,caller,   &
+                         !&  'trying to printout a property that is not mapped &
+                         !& to the particles',&
+                         !&  __LINE__,info)
+                    !GOTO 9999
+                 !ENDIF
+                 !IF (.NOT. prop%data_type.EQ.ppm_type_real .OR. &
+                        !prop%lda.LE.1) THEN 
+                         !info = ppm_error_error
+                         !CALL ppm_error(ppm_err_argument,caller,   &
+                     !&  'property should be a real vector field',&
+                             !&  __LINE__,info)
+                         !GOTO 9999
+                 !ENDIF
+                 !prop => NULL()
+              !ENDDO
+           !ELSE
+              !! dont print anything
+              !nb_wp_field = 0
+           !ENDIF
 
-           IF (.NOT. PRESENT(wp_list) .AND. .NOT. PRESENT(wp_field_list)) THEN
-               !printout all properties i that are mapped
-               nb_wp = Pc%props%nb
-               ALLOCATE(wpi_l(nb_wp),wps_l(nb_wp),wpv_l(nb_wp),STAT=info)
-               prop => Pc%props%begin()
-               DO WHILE (ASSOCIATED(prop))
-                   IF (prop%flags(ppm_ppt_partial)) THEN
-                       SELECT CASE (prop%data_type)
+           !IF (.NOT. PRESENT(wp_list) .AND. .NOT. PRESENT(wp_field_list)) THEN
+               !!printout all properties i that are mapped
+               !nb_wp = Pc%props%nb
+               !ALLOCATE(wpi_l(nb_wp),wps_l(nb_wp),wpv_l(nb_wp),STAT=info)
+               !prop => Pc%props%begin()
+               !DO WHILE (ASSOCIATED(prop))
+                   !IF (prop%flags(ppm_ppt_partial)) THEN
+                       !SELECT CASE (prop%data_type)
+                       !CASE (ppm_type_int)
+                           !IF (prop%lda.EQ.1) THEN
+                               !nb_wpi = nb_wpi + 1
+                               !wpi_l(nb_wpi) = Pc%props%iter_id
+                           !ENDIF
+
+                       !CASE (ppm_type_real)
+                           !IF (prop%lda.EQ.1) THEN
+                               !nb_wps = nb_wps + 1
+                               !wps_l(nb_wps) = Pc%props%iter_id
+                           !ELSE
+                               !nb_wpv = nb_wpv + 1
+                               !wpv_l(nb_wpv) = Pc%props%iter_id
+                           !ENDIF
+                       !CASE DEFAULT
+                           !!not a supported type for printout (yet)
+                       !END SELECT
+                       !prop => Pc%props%next()
+                   !ENDIF
+               !ENDDO
+           !ENDIF
+
+           IF (PRESENT(Field_list)) THEN
+               el => Field_list%begin()
+               DO WHILE (ASSOCIATED(field))
+                   SELECT TYPE(field => el)
+                   CLASS IS(ppm_t_field_)
+                       !hack, so that Pc%props%iter_id is now the id
+                       !of the discretization of field in Pc.
+                       CALL field%get_discr(Pc,discr_data,info)
+                       or_fail("could not get discr data for this field")
+                       SELECT CASE(field%lda)
                        CASE (ppm_type_int)
-                           IF (prop%lda.EQ.1) THEN
-                               nb_wpi = nb_wpi + 1
-                               wpi_l(nb_wpi) = Pc%props%iter_id
-                           ENDIF
-
+                           nb_wpi = nb_wpi + 1
+                           wpi_l(nb_wpi) = Pc%props%iter_id
                        CASE (ppm_type_real)
-                           IF (prop%lda.EQ.1) THEN
+                           IF (field%lda.EQ.1) THEN
                                nb_wps = nb_wps + 1
                                wps_l(nb_wps) = Pc%props%iter_id
                            ELSE
@@ -193,12 +228,13 @@
                                wpv_l(nb_wpv) = Pc%props%iter_id
                            ENDIF
                        CASE DEFAULT
-                           !not a supported type for printout (yet)
+                               !not a supported type for printout (yet)
                        END SELECT
-                       prop => Pc%props%next()
-                   ENDIF
+                   END SELECT
+                   el => Field_list%next()
                ENDDO
            ENDIF
+
 
 #ifdef __MPI
            ! write parallel file
