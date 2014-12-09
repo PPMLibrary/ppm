f91b77da9ae457029a09626fc11bdc932809ad83
Yaser <afshar@mpi-cbg.de>
2014-12-09 15:55:14 +0100

    minor debug to resolve the conflicting issue in USE ONLY for intel compiler version 12.1.3

--- src/ppm_module_map_part.f
+++ src/ppm_module_map_part.f
@@ -1,5 +1,5 @@
       !--*- f90 -*--------------------------------------------------------------
-      !  Module       :              ppm_module_map_part_pop
+      !  Module       :              ppm_module_map_part
       !-------------------------------------------------------------------------
       ! Copyright (c) 2012 CSE Lab (ETH Zurich), MOSAIC Group (ETH Zurich),
       !                    Center for Fluid Dynamics (DTU)
@@ -43,121 +43,121 @@
       !!!
       !!! The module also holds the needed work arrays for these routines.
 
-         USE ppm_module_data, ONLY: ppm_kind_single,ppm_kind_double
-         USE ppm_module_topo_typedef
-         USE ppm_module_map_part_util, ONLY : ppm_map_part_eqdistrib,    &
-         &   ppm_map_part_cancel,ppm_map_part_get_sub,ppm_map_part_load, &
-         &   ppm_map_part_ring_shift,ppm_map_part_store
-         USE ppm_module_map_part_ghost, ONLY : ppm_map_part_ghost_get, &
-         &   ppm_map_part_ghost_pop,ppm_map_part_ghost_put
-         USE ppm_module_map_part_global,ONLY : ppm_map_part_global, &
-         &   ppm_map_part_remap
-         USE ppm_module_map_part_partial, ONLY : ppm_map_part_partial
-         IMPLICIT NONE
+      USE ppm_module_data, ONLY: ppm_kind_single,ppm_kind_double
+      USE ppm_module_topo_typedef
+      USE ppm_module_map_part_util, ONLY : ppm_map_part_eqdistrib,    &
+      &   ppm_map_part_cancel,ppm_map_part_get_sub,ppm_map_part_load, &
+      &   ppm_map_part_ring_shift,ppm_map_part_store
+      USE ppm_module_map_part_ghost, ONLY : ppm_map_part_ghost_get, &
+      &   ppm_map_part_ghost_pop,ppm_map_part_ghost_put
+      USE ppm_module_map_part_global,ONLY : ppm_map_part_global, &
+      &   ppm_map_part_remap
+      USE ppm_module_map_part_partial, ONLY : ppm_map_part_partial
+      IMPLICIT NONE
 
-         PRIVATE
+      PRIVATE
 
-         !----------------------------------------------------------------------
-         !  Work lists
-         !----------------------------------------------------------------------
-         REAL(ppm_kind_single), DIMENSION(:),   POINTER :: sends => NULL()
-         REAL(ppm_kind_single), DIMENSION(:),   POINTER :: recvs => NULL()
-         REAL(ppm_kind_double), DIMENSION(:),   POINTER :: sendd => NULL()
-         REAL(ppm_kind_double), DIMENSION(:),   POINTER :: recvd => NULL()
+      !----------------------------------------------------------------------
+      !  Work lists
+      !----------------------------------------------------------------------
+      REAL(ppm_kind_single), DIMENSION(:),   POINTER :: sends => NULL()
+      REAL(ppm_kind_single), DIMENSION(:),   POINTER :: recvs => NULL()
+      REAL(ppm_kind_double), DIMENSION(:),   POINTER :: sendd => NULL()
+      REAL(ppm_kind_double), DIMENSION(:),   POINTER :: recvd => NULL()
 
-         INTEGER,               DIMENSION(:),   POINTER :: nsend => NULL()
-         INTEGER,               DIMENSION(:),   POINTER :: nrecv => NULL()
-         INTEGER,               DIMENSION(:),   POINTER :: psend => NULL()
-         INTEGER,               DIMENSION(:),   POINTER :: precv => NULL()
-         INTEGER,               DIMENSION(:,:), POINTER :: pp    => NULL()
-         INTEGER,               DIMENSION(:,:), POINTER :: qq    => NULL()
-         INTEGER                                        :: old_nsendlist = 0
-         INTEGER                                        :: old_buffer_set = 0
+      INTEGER,               DIMENSION(:),   POINTER :: nsend => NULL()
+      INTEGER,               DIMENSION(:),   POINTER :: nrecv => NULL()
+      INTEGER,               DIMENSION(:),   POINTER :: psend => NULL()
+      INTEGER,               DIMENSION(:),   POINTER :: precv => NULL()
+      INTEGER,               DIMENSION(:,:), POINTER :: pp    => NULL()
+      INTEGER,               DIMENSION(:,:), POINTER :: qq    => NULL()
+      INTEGER                                        :: old_nsendlist = 0
+      INTEGER                                        :: old_buffer_set = 0
 
-         !----------------------------------------------------------------------
-         !  Define interfaces to ppm_map_part_pop
-         !----------------------------------------------------------------------
-         INTERFACE ppm_map_part_pop
-            ! scalar (1d) particle data
-            MODULE PROCEDURE ppm_map_part_pop_1dd
-            MODULE PROCEDURE ppm_map_part_pop_1ds
-            MODULE PROCEDURE ppm_map_part_pop_1di
-            MODULE PROCEDURE ppm_map_part_pop_1dl
-            MODULE PROCEDURE ppm_map_part_pop_1ddc
-            MODULE PROCEDURE ppm_map_part_pop_1dsc
+      !----------------------------------------------------------------------
+      !  Define interfaces to ppm_map_part_pop
+      !----------------------------------------------------------------------
+      INTERFACE ppm_map_part_pop
+         ! scalar (1d) particle data
+         MODULE PROCEDURE ppm_map_part_pop_1dd
+         MODULE PROCEDURE ppm_map_part_pop_1ds
+         MODULE PROCEDURE ppm_map_part_pop_1di
+         MODULE PROCEDURE ppm_map_part_pop_1dl
+         MODULE PROCEDURE ppm_map_part_pop_1ddc
+         MODULE PROCEDURE ppm_map_part_pop_1dsc
 
-            ! vector (2d) particle data
-            MODULE PROCEDURE ppm_map_part_pop_2dd
-            MODULE PROCEDURE ppm_map_part_pop_2ds
-            MODULE PROCEDURE ppm_map_part_pop_2di
-            MODULE PROCEDURE ppm_map_part_pop_2dl
-            MODULE PROCEDURE ppm_map_part_pop_2ddc
-            MODULE PROCEDURE ppm_map_part_pop_2dsc
-         END INTERFACE
+         ! vector (2d) particle data
+         MODULE PROCEDURE ppm_map_part_pop_2dd
+         MODULE PROCEDURE ppm_map_part_pop_2ds
+         MODULE PROCEDURE ppm_map_part_pop_2di
+         MODULE PROCEDURE ppm_map_part_pop_2dl
+         MODULE PROCEDURE ppm_map_part_pop_2ddc
+         MODULE PROCEDURE ppm_map_part_pop_2dsc
+      END INTERFACE
 
-         !----------------------------------------------------------------------
-         !  Define interfaces to ppm_map_part_push
-         !----------------------------------------------------------------------
-         INTERFACE ppm_map_part_push
-            ! scalar (1d) particle data
-            MODULE PROCEDURE ppm_map_part_push_1dd
-            MODULE PROCEDURE ppm_map_part_push_1ds
-            MODULE PROCEDURE ppm_map_part_push_1di
-            MODULE PROCEDURE ppm_map_part_push_1dl
-            MODULE PROCEDURE ppm_map_part_push_1ddc
-            MODULE PROCEDURE ppm_map_part_push_1dsc
+      !----------------------------------------------------------------------
+      !  Define interfaces to ppm_map_part_push
+      !----------------------------------------------------------------------
+      INTERFACE ppm_map_part_push
+         ! scalar (1d) particle data
+         MODULE PROCEDURE ppm_map_part_push_1dd
+         MODULE PROCEDURE ppm_map_part_push_1ds
+         MODULE PROCEDURE ppm_map_part_push_1di
+         MODULE PROCEDURE ppm_map_part_push_1dl
+         MODULE PROCEDURE ppm_map_part_push_1ddc
+         MODULE PROCEDURE ppm_map_part_push_1dsc
 
-            ! vector (2d) particle data
-            MODULE PROCEDURE ppm_map_part_push_2dd
-            MODULE PROCEDURE ppm_map_part_push_2ds
-            MODULE PROCEDURE ppm_map_part_push_2di
-            MODULE PROCEDURE ppm_map_part_push_2dl
-            MODULE PROCEDURE ppm_map_part_push_2ddc
-            MODULE PROCEDURE ppm_map_part_push_2dsc
-         END INTERFACE
-         !----------------------------------------------------------------------
-         !  Define interfaces to ppm_map_part_send
-         !----------------------------------------------------------------------
-         INTERFACE ppm_map_part_send
-            MODULE PROCEDURE ppm_map_part_send
-         END INTERFACE
+         ! vector (2d) particle data
+         MODULE PROCEDURE ppm_map_part_push_2dd
+         MODULE PROCEDURE ppm_map_part_push_2ds
+         MODULE PROCEDURE ppm_map_part_push_2di
+         MODULE PROCEDURE ppm_map_part_push_2dl
+         MODULE PROCEDURE ppm_map_part_push_2ddc
+         MODULE PROCEDURE ppm_map_part_push_2dsc
+      END INTERFACE
+      !----------------------------------------------------------------------
+      !  Define interfaces to ppm_map_part_send
+      !----------------------------------------------------------------------
+      INTERFACE ppm_map_part_send
+         MODULE PROCEDURE ppm_map_part_send
+      END INTERFACE
 
-         INTERFACE ppm_map_part_isend
-            MODULE PROCEDURE ppm_map_part_isend
-         END INTERFACE
+      INTERFACE ppm_map_part_isend
+         MODULE PROCEDURE ppm_map_part_isend
+      END INTERFACE
 
-         PUBLIC :: ppm_map_part_pop
-         PUBLIC :: ppm_map_part_push
-         PUBLIC :: ppm_map_part_send
-         PUBLIC :: ppm_map_part_isend
-         !----------------------------------------------------------------------
-         ! PUBLIC var from ppm_module_map_part_util
-         !----------------------------------------------------------------------
-         PUBLIC :: ppm_map_part_eqdistrib
-         PUBLIC :: ppm_map_part_cancel
-         PUBLIC :: ppm_map_part_get_sub
-         PUBLIC :: ppm_map_part_load
-         PUBLIC :: ppm_map_part_ring_shift
-         PUBLIC :: ppm_map_part_store
-         !----------------------------------------------------------------------
-         ! PUBLIC var from ppm_module_map_part_ghost
-         !----------------------------------------------------------------------
-         PUBLIC :: ppm_map_part_ghost_get
-         PUBLIC :: ppm_map_part_ghost_pop
-         PUBLIC :: ppm_map_part_ghost_put
-         !----------------------------------------------------------------------
-         ! PUBLIC var from ppm_module_map_part_global
-         !----------------------------------------------------------------------
-         PUBLIC :: ppm_map_part_global
-         PUBLIC :: ppm_map_part_remap
-         !----------------------------------------------------------------------
-         ! PUBLIC var from ppm_module_map_part_partial
-         !----------------------------------------------------------------------
-         PUBLIC :: ppm_map_part_partial
-         !----------------------------------------------------------------------
-         !  include the source
-         !----------------------------------------------------------------------
-         CONTAINS
+      PUBLIC :: ppm_map_part_pop
+      PUBLIC :: ppm_map_part_push
+      PUBLIC :: ppm_map_part_send
+      PUBLIC :: ppm_map_part_isend
+      !----------------------------------------------------------------------
+      ! PUBLIC var from ppm_module_map_part_util
+      !----------------------------------------------------------------------
+      PUBLIC :: ppm_map_part_eqdistrib
+      PUBLIC :: ppm_map_part_cancel
+      PUBLIC :: ppm_map_part_get_sub
+      PUBLIC :: ppm_map_part_load
+      PUBLIC :: ppm_map_part_ring_shift
+      PUBLIC :: ppm_map_part_store
+      !----------------------------------------------------------------------
+      ! PUBLIC var from ppm_module_map_part_ghost
+      !----------------------------------------------------------------------
+      PUBLIC :: ppm_map_part_ghost_get
+      PUBLIC :: ppm_map_part_ghost_pop
+      PUBLIC :: ppm_map_part_ghost_put
+      !----------------------------------------------------------------------
+      ! PUBLIC var from ppm_module_map_part_global
+      !----------------------------------------------------------------------
+      PUBLIC :: ppm_map_part_global
+      PUBLIC :: ppm_map_part_remap
+      !----------------------------------------------------------------------
+      ! PUBLIC var from ppm_module_map_part_partial
+      !----------------------------------------------------------------------
+      PUBLIC :: ppm_map_part_partial
+      !----------------------------------------------------------------------
+      !  include the source
+      !----------------------------------------------------------------------
+      CONTAINS
 
 #define __DIM 1
 #define __KIND __SINGLE_PRECISION
